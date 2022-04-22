143421c6ee7842082ec20506632f07a954064592
Kirby Nguyen <kirby.ducnguyen@outlook.com>
2022-04-21 23:01:37 -0700

    Added password protection check for journal

--- lib/pages/journal_list_page.dart
+++ lib/pages/journal_list_page.dart
@@ -1,14 +1,244 @@
-import 'package:diary_journals/controllers/entry_controller.dart';
-import 'package:diary_journals/pages/entry_list_page.dart';
-import 'package:diary_journals/pages/journal_info_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
+import 'package:diary_journals/models/journal.dart';
+import 'package:diary_journals/pages/entry_list_page.dart';
+import 'package:diary_journals/pages/journal_info_page.dart';
+import 'package:diary_journals/controllers/entry_controller.dart';
 import 'package:diary_journals/controllers/journal_controller.dart';
 
-class JournalListPage extends ConsumerWidget {
+class JournalListPage extends HookConsumerWidget {
   const JournalListPage({Key? key}) : super(key: key);
+
+  Future<void> showDeleteDialog(
+    BuildContext context,
+    WidgetRef ref,
+    Journal journal,
+  ) async {
+    final GlobalKey<FormState> passwordKey = GlobalKey<FormState>();
+    final TextEditingController controller = TextEditingController();
+
+    return await showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              content: Form(
+                key: passwordKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Enter the password"),
+                    TextFormField(
+                      obscureText: true,
+                      controller: controller,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              title: const Text("Password require"),
+              actions: <Widget>[
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    "CANCEL",
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xFFFE4A49),
+                  ),
+                  onPressed: () {
+                    if (passwordKey.currentState!.validate()) {
+                      if (controller.text == journal.password) {
+                        ref.watch(journalController.notifier).deleteJournal(
+                              journal.id,
+                            );
+                        Navigator.of(context).pop();
+                      }
+                    }
+                  },
+                  child: const Text(
+                    "DELETE",
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<void> showEditDialog(
+    BuildContext context,
+    WidgetRef ref,
+    Journal journal,
+  ) async {
+    final GlobalKey<FormState> passwordKey = GlobalKey<FormState>();
+    final TextEditingController controller = TextEditingController();
+
+    return await showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              content: Form(
+                key: passwordKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Enter the password"),
+                    TextFormField(
+                      controller: controller,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              title: const Text("Password require"),
+              actions: <Widget>[
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    "CANCEL",
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(255, 255, 196, 0),
+                  ),
+                  onPressed: () {
+                    if (passwordKey.currentState!.validate()) {
+                      if (controller.text == journal.password) {
+                        Navigator.of(context).pop();
+                        Navigator.push<void>(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) => JournalInfoPage(
+                              journal: journal,
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text(
+                    "EDIT",
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<void> showConfirmDialog(
+    BuildContext context,
+    WidgetRef ref,
+    Journal journal,
+  ) async {
+    final GlobalKey<FormState> passwordKey = GlobalKey<FormState>();
+    final TextEditingController controller = TextEditingController();
+
+    return await showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              content: Form(
+                key: passwordKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Enter the password"),
+                    TextFormField(
+                      controller: controller,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              title: const Text("Password require"),
+              actions: <Widget>[
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    "CANCEL",
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (passwordKey.currentState!.validate()) {
+                      if (controller.text == journal.password) {
+                        ref
+                            .watch(entryController.notifier)
+                            .entryInit(journal.id);
+                        Navigator.of(context).pop();
+                        Navigator.push<void>(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) => EntryListPage(
+                              journal: journal,
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text(
+                    "CONFIRM",
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -38,10 +268,15 @@ class JournalListPage extends ConsumerWidget {
               motion: const ScrollMotion(),
               children: [
                 SlidableAction(
-                  onPressed: (context) {
-                    ref
-                        .watch(journalController.notifier)
-                        .deleteJournal(ref.watch(journalController)[index].id);
+                  onPressed: (context) async {
+                    if (ref.read(journalController)[index].isPrivate != 0) {
+                      showDeleteDialog(
+                          context, ref, ref.read(journalController)[index]);
+                    } else {
+                      ref.watch(journalController.notifier).deleteJournal(
+                            ref.read(journalController)[index].id,
+                          );
+                    }
                   },
                   backgroundColor: const Color(0xFFFE4A49),
                   foregroundColor: Colors.white,
@@ -50,14 +285,19 @@ class JournalListPage extends ConsumerWidget {
                 ),
                 SlidableAction(
                   onPressed: (context) {
-                    Navigator.push<void>(
-                      context,
-                      MaterialPageRoute<void>(
-                        builder: (BuildContext context) => JournalInfoPage(
-                          journal: ref.watch(journalController)[index],
+                    if (ref.read(journalController)[index].isPrivate != 0) {
+                      showEditDialog(
+                          context, ref, ref.read(journalController)[index]);
+                    } else {
+                      Navigator.push<void>(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (BuildContext context) => JournalInfoPage(
+                            journal: ref.watch(journalController)[index],
+                          ),
                         ),
-                      ),
-                    );
+                      );
+                    }
                   },
                   backgroundColor: const Color.fromARGB(255, 255, 196, 0),
                   foregroundColor: Colors.white,
@@ -70,10 +310,15 @@ class JournalListPage extends ConsumerWidget {
               motion: const ScrollMotion(),
               children: [
                 SlidableAction(
-                  onPressed: (context) {
-                    ref
-                        .watch(journalController.notifier)
-                        .deleteJournal(ref.watch(journalController)[index].id);
+                  onPressed: (context) async {
+                    if (ref.read(journalController)[index].isPrivate != 0) {
+                      showDeleteDialog(
+                          context, ref, ref.read(journalController)[index]);
+                    } else {
+                      ref.watch(journalController.notifier).deleteJournal(
+                            ref.read(journalController)[index].id,
+                          );
+                    }
                   },
                   backgroundColor: const Color(0xFFFE4A49),
                   foregroundColor: Colors.white,
@@ -82,14 +327,19 @@ class JournalListPage extends ConsumerWidget {
                 ),
                 SlidableAction(
                   onPressed: (context) {
-                    Navigator.push<void>(
-                      context,
-                      MaterialPageRoute<void>(
-                        builder: (BuildContext context) => JournalInfoPage(
-                          journal: ref.watch(journalController)[index],
+                    if (ref.read(journalController)[index].isPrivate != 0) {
+                      showEditDialog(
+                          context, ref, ref.read(journalController)[index]);
+                    } else {
+                      Navigator.push<void>(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (BuildContext context) => JournalInfoPage(
+                            journal: ref.watch(journalController)[index],
+                          ),
                         ),
-                      ),
-                    );
+                      );
+                    }
                   },
                   backgroundColor: const Color.fromARGB(255, 255, 196, 0),
                   foregroundColor: Colors.white,
@@ -100,17 +350,22 @@ class JournalListPage extends ConsumerWidget {
             ),
             child: GestureDetector(
               onTap: () {
-                ref
-                    .watch(entryController.notifier)
-                    .entryInit(ref.watch(journalController)[index].id);
-                Navigator.push<void>(
-                  context,
-                  MaterialPageRoute<void>(
-                    builder: (BuildContext context) => EntryListPage(
-                      journal: ref.watch(journalController)[index],
+                if (ref.read(journalController)[index].isPrivate != 0) {
+                  showConfirmDialog(
+                      context, ref, ref.read(journalController)[index]);
+                } else {
+                  ref
+                      .watch(entryController.notifier)
+                      .entryInit(ref.watch(journalController)[index].id);
+                  Navigator.push<void>(
+                    context,
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => EntryListPage(
+                        journal: ref.watch(journalController)[index],
+                      ),
                     ),
-                  ),
-                );
+                  );
+                }
               },
               child: ListTile(
                 trailing: ref.watch(journalController)[index].isPrivate == 1
